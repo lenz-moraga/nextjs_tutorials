da60e360d397c7816edf016adf8983f16524579a
lmoraga505 <49219425+lenz-moraga@users.noreply.github.com>
2023-10-10 08:54:35 -0600

    add graphql to get the episodes information

--- my-first-nextjs-project/app/episodes/page.jsx
+++ my-first-nextjs-project/app/episodes/page.jsx
@@ -1,18 +1,34 @@
-"use client";
+// "use client";
 
-import useFetch from "@/hooks/useFetch";
 import Link from "next/link";
 
-export default function Episodes() {
-  const { data, error } = useFetch("/episode");
+import { getClient } from "@/lib/client";
+import { gql } from "@apollo/client";
+
+const query = gql`
+  query {
+    episodes(page: 1, filter: { name: "" }) {
+      info {
+        count
+      }
+      results {
+        name
+      }
+    }
+  }
+`;
+
+export default async function Episodes() {
+  const { data, error } = await getClient().query({ query });
+  const { results: episodes } = data.episodes;
 
   return (
     <>
       <h2>Episodes</h2>
       {error && <div>Error: {error.message}</div>}
-      {data && data.results && (
+      {episodes && (
         <ul>
-          {data.results.map((episode) => (
+          {episodes.map((episode) => (
             <li key={episode.id}>
               <Link href={`/episodes/episode_details/${episode.id}`}>
                 {episode.name}
